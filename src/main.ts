bfb0e5e02705ff88c8c5c17cfe3d35a6fb3483fe
Felipe <felima@brasilseg.com.br>
2024-05-05 20:37:29 -0300

    feat: main security and cors setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+
+const PORT = parseInt(process.env.SERVER_PORT, 10 || 3000);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,PUT,POST,PATCH,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: false,
+    maxAge: 3600,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.use(helmet());
+
+  await app.listen(PORT);
+
+  Logger.log(`Server running on https://localhost:${PORT}`, 'Boostrap');
 }
 bootstrap();
